511b5057e386a51ecb41fb73ee54251f88c2dc95
Voyager214 <214662467@qq.com>
2025-06-29 13:58:20 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/FT Study Group', '/Private'],
+  protectedRoutes: ['/未公开', '/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
